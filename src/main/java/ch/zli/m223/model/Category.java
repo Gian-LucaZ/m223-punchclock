b8e8a5328c762de4ca69ab45485e9cfee2bb2153
GianLuca <gianluca.zwahlen@cmiag.ch>
2023-10-26 09:12:15 +0000

    feat: Add tags and category

---
+++ src/main/java/ch/zli/m223/model/Category.java
@@ -0,0 +1,38 @@
+package ch.zli.m223.model;
+
+import java.util.List;
+
+import javax.persistence.*;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    @JsonIgnoreProperties("category")
+    @Fetch(FetchMode.JOIN)
+    private List<Entry> entries;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ch/zli/m223/model/Tag.java
@@ -0,0 +1,32 @@
+package ch.zli.m223.model;
+
+import java.util.List;
+
+import javax.persistence.*;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Entry> taggedEntries;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
